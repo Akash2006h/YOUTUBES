819d80c03688004fc3d1ce4fa68efb8e4633f0d4
Akash2006h <akashkarki352@gmail.com>
2025-04-18 00:06:14 +0530

    make an dynamic code for button list

---
+++ src/Components/Button.jsx
@@ -0,0 +1,9 @@
+
+const Button = ({label}) =>{
+  return(
+  <div>
+      <button className = "p-3 m-2 py-2 bg-gray-200 rounded-lg">{label}</button>
+  </div>
+  )
+}
+export default Button;
--- src/Components/ButtonList.jsx
+++ src/Components/ButtonList.jsx
@@ -1,6 +1,14 @@
+import Button from "./Button.jsx"
+const List = ["All","Tranding","Sports","Dancing","Songs","Movies","Cooking","....."]
 const ButtonList = () =>{
 return(
-<div>ButtonList</div>
+<div className = "flex" >
+      {
+        List.map((item, index) =>(
+        <Button key={index} label = {item} />
+        ))
+      }
+    </div>
 )
 } 
 export default ButtonList;
